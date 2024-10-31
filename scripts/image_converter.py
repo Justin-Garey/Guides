fea6f08581c2f4e618c058e587d59f174c4ee662
Justin Garey <77745791+Justin-Garey@users.noreply.github.com>
2024-10-31 16:31:04 -0400

    Update image_converter.py to work with HEIC and HEIF format

--- scripts/image_converter.py
+++ scripts/image_converter.py
@@ -3,16 +3,25 @@
 import os
 import argparse
 from PIL import Image
+from pillow_heif import register_heif_opener
+
+# Requirements
+# pip3 install pillow-heif pillow argparse
 
 FILETYPES = ('.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp', '.heic', '.png')
+CONVERT_FILETYPES = ('.heic', '.heif')
 
 def convert_images(directory, filetype, save):
     for filename in os.listdir(directory):
         if filename.lower().endswith(FILETYPES) and not filename.lower().endswith(('.' + filetype)): 
             file_path = os.path.join(directory, filename)
+            if filename.lower().endswith(CONVERT_FILETYPES):
+                register_heif_opener()
             with Image.open(file_path) as img:
                 png_filename = os.path.splitext(filename)[0] + '.' + filetype
                 png_file_path = os.path.join(directory, png_filename)
+                if filename.lower().endswith(CONVERT_FILETYPES):
+                    img = img.convert('RGB')
                 img.save(png_file_path)
             if not save:
                 os.remove(file_path)  # Remove the original file
